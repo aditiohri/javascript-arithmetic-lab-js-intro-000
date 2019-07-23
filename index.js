c8cfefd645b6006a948a63b6b05102b9b460ee1f
Aditi Ohri <didiohri@gmail.com>
2019-07-23 01:01:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ function divide(a, b) {
   number = a / b;
   return number;
 }
-function inc(n) {
+function inc(a) {
   number = a + 1;
   return number;
 }
-function dec(n) {
+function dec(a) {
   number = a - 1;
   return number;
 }
-function makeInt(n) {
+function makeInt(a) {
   number = parseInt(a, 10)
   return number;
 }
-function preserveDecimal(n) {
-  number = parseFloat(a);
+function preserveDecimal(a) {
+  number = parseFloat(a , 10);
   return number;
 }
